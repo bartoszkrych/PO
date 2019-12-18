a5455fd50c1f909631a550a602303d89fffcdee3
Bartosz <bartuskr@gmail.com>
2019-12-18 23:13:00 +0100

    Initial Spring App

---
+++ w-gory-java/src/test/java/com/wGory/WGoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wGory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WGoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
