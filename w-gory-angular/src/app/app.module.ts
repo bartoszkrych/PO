83f2986335ea34f546f8f5ac0274329199b0ddda
Bartosz <bartuskr@gmail.com>
2019-12-23 12:14:40 +0100

    Added providers

--- w-gory-angular/src/app/app.module.ts
+++ w-gory-angular/src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MiejsceService } from './controller/service/miejsce.service';
+import { TrasaPunktowanaService } from './controller/service/trasa-punktowana.service';
+import { WycieczkaService } from './controller/service/wycieczka.service';
 
 @NgModule({
   declarations: [
@@ -10,9 +15,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [MiejsceService, TrasaPunktowanaService, WycieczkaService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
