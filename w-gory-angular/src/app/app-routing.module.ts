4b4816f56379163ba430a7026625965e084cd739
Bartosz <bartuskr@gmail.com>
2019-12-28 17:58:00 +0100

    Added routing to new component

--- w-gory-angular/src/app/app-routing.module.ts
+++ w-gory-angular/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ZatwierdzenieWycieczkiComponent } from './component/zatwierdzenie-wycieczki/zatwierdzenie-wycieczki.component';
+import { TworzenieTrasyPunktowanejComponent } from './component/tworzenie-trasy-punktowanej/tworzenie-trasy-punktowanej.component';
 
 
 const routes: Routes = [
-  {path:'wycieczki', component: ZatwierdzenieWycieczkiComponent}
+  {path:'wycieczki', component: ZatwierdzenieWycieczkiComponent},
+  {path:'trasy-punktowane', component: TworzenieTrasyPunktowanejComponent}
 ];
 
 @NgModule({
