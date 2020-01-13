2891285d4c1820681df9c0e9528a627e3bc0f1fa
Bartosz <bartuskr@gmail.com>
2020-01-13 16:27:51 +0100

    Remove useless methods

--- w-gory-java/src/main/java/com/wGory/controller/repository/MiejsceRepository.java
+++ w-gory-java/src/main/java/com/wGory/controller/repository/MiejsceRepository.java
@@ -4,9 +4,6 @@ import com.wGory.model.Miejsce;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface MiejsceRepository extends JpaRepository<Miejsce, Integer> {
-    List<Miejsce> findAll();
 }
--- w-gory-java/src/main/java/com/wGory/controller/repository/WycieczkaRepository.java
+++ w-gory-java/src/main/java/com/wGory/controller/repository/WycieczkaRepository.java
@@ -4,9 +4,6 @@ import com.wGory.model.Wycieczka;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface WycieczkaRepository extends JpaRepository<Wycieczka, Integer> {
-    Optional<Wycieczka> findById(Integer id);
 }
